bbe8b3906d5c116a376d1ad974f0663012083d9d
jeason <chenzhang7618@163.com>
2019-10-08 16:17:41 +0800

    完成容量运算

--- src/pages/sportActionGroup/MyChoiceAction.jsx
+++ src/pages/sportActionGroup/MyChoiceAction.jsx
@@ -3,6 +3,8 @@ import Card from 'component/card/MediaCard.jsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import store from 'redux/stors/MyChoiceActionStore';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 const useStyles = makeStyles({
   roots: {
     height: '100%',
@@ -12,12 +14,26 @@ const useStyles = makeStyles({
 export default function MyChoiceAction(props) {
   const classes = useStyles();
   const myChoice = store.getState();
+  const [exciseVolume, setExciseVolume] = React.useState(0);
+  let mynum = 0;
+  React.useEffect(() => {
+    myChoice.map(cur => {
+      console.log(cur);
+      mynum = cur.numGroup * cur.kilo + mynum;
+    });
+    setExciseVolume(mynum);
+  });
   console.log(myChoice);
   return (
     <Container className={classes.roots}>
       {myChoice.map((cur, index) => (
         <Card key={index} obj={cur}></Card>
       ))}
+      <Typography component="div">
+        <Box fontStyle="italic" m={1}>
+          您今天的训练容量{exciseVolume}kg
+        </Box>
+      </Typography>
     </Container>
   );
 }
